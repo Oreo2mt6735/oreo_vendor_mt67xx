a3ea75c3107709565b521ac0e119c1bed5b1a345
iykequame <zac6ix@gmail.com>
2017-09-25 09:04:25 +0000

    revert for O | 25/07/17

--- config/common.mk
+++ config/common.mk
@@ -20,38 +20,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcurl
 
-# Chromium
-PRODUCT_PACKAGES += \
-    Chromium
-
-# OmniStyle
-PRODUCT_PACKAGES += \
-    OmniStyle
-
-# OmniJaws
-PRODUCT_PACKAGES += \
-    OmniJaws
-
-# madCamera
-PRODUCT_PACKAGES += \
-    madCamera
-
-# madLauncher
-PRODUCT_PACKAGES += \
-    madLauncher
-
-# madGallery
-PRODUCT_PACKAGES += \
-    madGallery
-
 # Include explicitly to work around Facelock issues
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full
 
 # FMRadio
-PRODUCT_PACKAGES += \
-    libfmjni \
-    FMRadio
+#PRODUCT_PACKAGES += \
+    #libfmjni \
+    #FMRadio
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
@@ -81,25 +57,9 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     mad_charger_res_images
 
-# WallpaperPicker
-PRODUCT_PACKAGES += \
-    WallpaperPicker
-
 # Sensor Calibration
 PRODUCT_PACKAGES += \
     libem_sensor_jni
-
-# ThemeInterfacer
-PRODUCT_PACKAGES += \
-    ThemeInterfacer
-
-# Eleven
-#PRODUCT_PACKAGES += \
-    #Eleven
-
-# Bootanimation
-#PRODUCT_COPY_FILES += \
-    #vendor/mt67xx/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
@@ -107,30 +67,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.vc_call_vol_steps=14 \
     ro.config.media_vol_steps=30
-
-# SELinux
-#PRODUCT_PROPERTY_OVERRIDES += \
-    #ro.build.selinux=1
-
-# madOS Versioning
-#ANDROID_VERSION = 7.1.2
-#MADOS_VERSION = 1.2
-
-#ifndef MADOS_BUILD_TYPE
-    #MADOS_BUILD_TYPE := OFFICIAL
-#endif
-
-#MADOS_MOD_VERSION := madOS-$(MADOS_VERSION)-$(shell date -u +%Y%m%d)-$(MADOS_BUILD_TYPE)
-
-#PRODUCT_PROPERTY_OVERRIDES += \
-    #ro.mados.version=$(MADOS_VERSION) \
-    #ro.mados.releasetype=$(MADOS_BUILD_TYPE) \
-    #ro.modversion=$(MADOS_MOD_VERSION)
-
-#MADOS_DISPLAY_VERSION := madOS-$(MADOS_VERSION)-$(MADOS_BUILD_TYPE)
-
-#PRODUCT_PROPERTY_OVERRIDES += \
-    #ro.mados.display.version=$(MADOS_DISPLAY_VERSION)
 
 # include other configs
 include vendor/mt67xx/config/permissions.mk
